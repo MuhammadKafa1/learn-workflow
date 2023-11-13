86ab9ffc3eda5e7a8875f1a5b4e6b47d7b7a9455
Muhammad Kafa <muhammad.kafa@ingka.com>
2023-11-13 14:13:26 +0100

    Anpassung message.sh comments

--- scripts/conventional-commits/message.sh
+++ scripts/conventional-commits/message.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Validates a commit message against a sub-set of the Conventional Commits
+# Validates a commit message and PR title against a sub-set of the Conventional Commits
 # specification https://www.conventionalcommits.org
 #
 # Supported features
@@ -8,13 +8,13 @@
 #  - optional scope
 #  - breaking changes are declared using the ! character in the commit subject
 #
-# Example of valid commit messages
+# Example of valid commit messages and PR title
 #  - feat: add something
 #  - fix(docs): typo in README
 #  - feat(api)!: change the parameter order in the api
 #  - fix(api): fixed broken import (#123)
 #
-# Example of invalid commit messages
+# Example of invalid commit messages and PR title
 #  - did some work
 #  - feat:add support for something
 #  - hack: did my own thing
@@ -29,7 +29,7 @@ VALID_MESSAGE_FORMAT="^($SUPPORTED_TYPES)(\([a-z]+\))?!?: [a-zA-Z0-9\., \/_@()#-
 
 # Validate message.
 if ! [[ "$1" =~ $VALID_MESSAGE_FORMAT ]]; then
-    printf "❌ Invalid commit message\n%s" "$1"
+    printf "❌ Invalid PR title\n%s" "$1"
 
     exit 1
 fi
