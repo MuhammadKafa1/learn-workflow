2db2b9e9c0bfdbe6e4e634a6b70311e63ac32070
Muhammad Kafa <muhammad.kafa@ingka.com>
2024-02-19 17:08:14 +0100

    ci: some test

--- scripts/conventional-commits/test.sh
+++ scripts/conventional-commits/test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo "version=$(scripts/conventional-commits/version.sh)"
+version=$(scripts/conventional-commits/version.sh)
+
+
 if [ $? -eq 0 ]; then
   echo "Copy successful"
 else
   echo "Copy failed"
 fi
+
+echo "version is $version"
